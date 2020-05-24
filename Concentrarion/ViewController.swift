171ad5ca2ac46b35c72b16a07cae8fcc9465cadf
maximusnikulin <perseusxii@gmail.com>
2020-05-24 15:22:40 +0700

    Initial Commit

---
+++ Concentrarion/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Concentrarion
+//
+//  Created by Max Nikulin on 24.05.2020.
+//  Copyright © 2020 Max Nikulin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
