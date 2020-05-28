12eef03c138b8b842d20730e39d7d8eb175e817b
maximusnikulin <perseusxii@gmail.com>
2020-05-28 23:48:43 +0700

    data driven way

--- Concentrarion/ViewController.swift
+++ Concentrarion/ViewController.swift
@@ -9,12 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    var flipCounter: Int = 0 {
+        didSet {
+            flipCounterLabel.text = String("FlipCounts: \(flipCounter)")
+        }
     }
-
-
+    
+    @IBOutlet var cardsButtons: [UIButton]!
+    
+    var emojiCards: [String] = ["👻", "🎃", "👻", "🎃"]
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        if let indexCard = cardsButtons.firstIndex(of: sender) {
+            let emoji = emojiCards[indexCard]
+            flipCard(withEmoji: emoji, on: sender)
+        } else {
+            print("no cards with this emoji")
+        }
+    }
+    
+    @IBOutlet weak var flipCounterLabel: UILabel!
+    
+    func flipCard(withEmoji emoji: String, on button: UIButton) -> Void {
+        flipCounter += 1
+        if button.currentTitle == emoji {
+            button.setTitle("", for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 0.5825584531, blue: 0, alpha: 1)
+        } else {
+            button.setTitle(emoji, for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        }
+    }
 }
 
